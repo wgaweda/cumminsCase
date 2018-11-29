1b57d75f7ae7ff7092bc948084f8a76e621f2a65
wgaweda <wgaweda@iu.edu>
2018-11-29 14:59:28 -0500

    j

---
+++ app/models/Client.php
@@ -0,0 +1,53 @@
+<?php
+
+class Client
+
+{
+  public $clientId;
+  public $clientName;
+  public $sector;
+  public $headquarters;
+
+  public function __construct($data) {
+  $this->clientId = isset($data['clientId']) ? intval($data['clientId']) : null;
+  $this->clientName = $data['clientName'];
+  $this->sector = $data['sector'];
+  $this->headquarters = $data['headquarters'];
+
+}
+  // public static function fetchAll() {
+  //   $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+  //   //2. run a query
+  //   $sql = 'SELECT * FROM client';
+  //   $statement = $db->prepare($sql);
+  //   //3. read the results
+  //   $success = $statement->execute();
+  //   //4. handle the results
+  //   $arr = [];
+  //   while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+  //     $theClientList = new Client($row);
+  //     array_push($arr, $theClientList);
+  //   }
+  //   return $arr;
+  // }
+
+  public static function fetchByClientId(int $clientId) {
+//trying this j
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+
+    //2. run a query
+    $sql = 'SELECT * FROM client WHERE clientId = ?';
+    $statement = $db->prepare($sql);
+    //3. read the results
+    $success = $statement->execute(
+      [$clientId]
+    );
+    //4. handle the results
+    $arr = [];
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+      $theClient = new Client($row);
+      array_push($arr, $theClient);
+    }
+    return $arr;
+  }
+}
---
+++ public/api/client.php
@@ -0,0 +1,15 @@
+<?php
+require '../../app/common.php';
+
+$clientId = intval($_GET['clientId'] ?? 0);
+
+if ($clientId < 1) {
+  throw new Exception('Invalid Client ID in URL');
+}
+
+$client = Client::fetchByClientId($clientId);
+
+$json = json_encode($client, JSON_PRETTY_PRINT);
+
+header('Content-Type: application/json');
+echo $json;
