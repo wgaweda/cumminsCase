d6a2448612de6c3e5645889df450d7a340110281
wgaweda <wgaweda@iu.edu>
2018-11-29 15:02:16 -0500

    git out bruhhhh

---
+++ app/common.php
@@ -0,0 +1,22 @@
+<?php
+
+//Change the working directory to this file
+chdir( __DIR__ );
+set_include_path ( __DIR__ );
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'
+&& stripos($_SERVER['CONTENT_TYPE'], 'application/json') !== false ) {
+  $_POST = json_decode(file_get_contents('php://input'), true);
+}
+
+require 'environment.php';
+
+/** models **/
+require 'models/Client.php';
+require 'models/Sensor.php';
+require 'models/SensorDeployed.php';
+require 'models/SensorTimeSeries.php';
+require 'models/Site.php';
+require 'models/Turbine.php';
+require 'models/TurbineDeployed.php';
+require 'models/ClientNotes.php';
