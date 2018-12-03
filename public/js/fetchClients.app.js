ea862763250b695b994cc2c091d91a94c3a4fd08
wgaweda <wgaweda@iu.edu>
2018-12-03 13:53:31 -0500

    n

--- public/js/fetchClients.app.js
+++ public/js/fetchClients.app.js
@@ -6,21 +6,22 @@ var cumminsApp = new Vue({
 },
 methods: {
 
-  fetchCummins () {
-  fetch('api/allClient.php')
-    .then( response => response.json() )
-    .then( json => {cumminsApp.clientList = json; console.log(cumminsApp.clientList)} )
-    .catch( err => {
-      console.log('CLIENT LIST ERROR:');
-      console.log(err);
-    })
-
-  },
-
   gotoClient (cid) {
     window.location = 'clientEngines.html?clientId=' + cid;
   },
 
-}
+},
+
+created () {
+
+fetch('api/allClient.php')
+  .then( response => response.json() )
+  .then( json => {cumminsApp.clientList = json; console.log(cumminsApp.clientList)} )
+  .catch( err => {
+    console.log('CLIENT LIST ERROR:');
+    console.log(err);
+  })
+
+},
 
 })
