519251acfebf4a87a75545ab2d106ccba3176254
wgaweda <wgaweda@iu.edu>
2018-12-03 10:07:10 -0500

    f

---
+++ public/js/cummins.app.js
@@ -0,0 +1,42 @@
+var clientApp = new Vue({
+  el: '#clientsMain',
+  data:{
+    clients: []
+
+},
+methods: {
+
+  fetchClient(cid) {
+  fetch('api/client.php?clientId='+cid)
+  .then( response => response.json() )
+  .then( json => {this.clients = json; console.log(this.clients)} )
+  .catch( err => {
+      console.log('CLIENT FETCH ERROR:');
+      console.log(err);
+    })
+  },
+
+
+  gotoClient (cid) {
+    window.location = 'client.html?clientId=' + cid;
+  },
+
+  },
+  created() {
+    const url = new URL(window.location.href);
+    const clientId = url.searchParams.get('clientId') || 0;
+    console.log('Client: '+ clientId);
+    this.clients.clientId = clientId;
+
+    if (!clientId) {
+      console.error('Client Id not defined in URL parameters.')
+    }
+
+
+    this.fetchClient(clientId);
+    this.fetchNotes(clientId);
+    this.fetchSite(clientId);
+  }
+
+
+})
