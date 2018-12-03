defc2cf06d2b7da88042af469055eaee1761fddb
wgaweda <wgaweda@iu.edu>
2018-12-03 14:27:02 -0500

    f

--- public/js/cummins.app.js
+++ public/js/cummins.app.js
@@ -67,21 +67,5 @@ methods: {
 
   },
 
-  created () {
-    const url = new URL(window.location.href);
-    const engineDeployedId = url.searchParams.get('engineDeployedId') || 0;
-    console.log('Engine Deployed ID: '+ engineDeployedId);
-    this.deployed.engineDeployedId = engineDeployedId;
-
-
-    fetch('api/timeSeries.php?engineDeployedId='+engineDeployedId)
-    .then( response => response.json() )
-    .then( json => {this.series = json; console.log(this.series)} )
-    .catch( err => {
-        console.log('TIME SERIES FETCH ERROR:');
-        console.log(err);
-      })
-    }
-
 
 })
